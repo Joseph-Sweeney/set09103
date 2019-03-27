5616ef4864243a712499c43920ef1ca0407d26be
Joseph-Sweeney <josephasweeney@hotmail.co.uk>
2019-03-27 00:40:27 +0000

    Flask Blog web-app for Group Project module

---
+++ Flask_Blog/flaskblog/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'flaskblogtestemail@gmail.com'
+    MAIL_PASSWORD = '}?XMhn4GyJ&9E-zX'
